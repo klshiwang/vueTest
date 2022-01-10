2b3958c92ac46a95f1d535f4f01c50d45b51f52b
Hyson <klshiwang@126.com>
2022-01-10 12:31:08 +0800

    merge

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
+
 module.exports={
 
   devServer:{
     port:5555,
     open:true
   }
-=======
-module.exports = {
-    devServer:{
-        port:5555
-    }
->>>>>>> 0d54c22f451b77711c25d388bc41657bac3dc234
 }
